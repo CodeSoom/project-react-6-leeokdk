aa76390a08fac649ed42fad5f6a798aa81f3f628
leeok <milkteatea0628@gmail.com>
2022-01-24 23:38:15 +0900

    기본적인 개발환경 셋팅

---
+++ src/slice.js
@@ -0,0 +1,11 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const { actions, reducer } = createSlice({
+  name: 'application',
+  initialState: {
+  },
+  reducers: {
+  },
+});
+
+export default reducer;
